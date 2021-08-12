9947f8a73e61624c44a5e6d31f7581e13609f7e3
mixmax0M <50010733+mixmax0M@users.noreply.github.com>
2021-08-12 07:53:18 +0200

    Add files via upload

---
+++ Trainer Framework/pch.h
@@ -0,0 +1,37 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+// add headers that you want to pre-compile here
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <utility>
+
+// Data structures
+#include <string>
+#include <stack>
+#include <deque>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+
+// Windows API
+#include <Windows.h>
+#include <TlHelp32.h>
+
+// More stuff
+#include <stdio.h>
+#include <tchar.h>
+
+#endif //PCH_H
